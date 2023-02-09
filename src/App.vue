a0bf1460e72dd80e22312435cfc73dc0c8655b4a
rcidral <ricidralmfo@gmail.com>
2023-02-09 14:15:54 -0300

    adding cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
 import heading from "./components/heading.vue";
 import footerinfo from "./components/footer.vue";
+import cardsinfo from "./components/cards.vue";
 import headerinfo from "./components/header.vue";
 export default {
-  components: { headerinfo, heading, footerinfo },
+  components: { headerinfo, heading, footerinfo, cardsinfo },
   data() {
     return {};
   },
@@ -14,6 +15,7 @@ export default {
   <div class="bg-neutral-900 justify-center">
     <headerinfo></headerinfo>
     <heading></heading>
+    <cardsinfo></cardsinfo>
     <footerinfo></footerinfo>
   </div>
 </template>
