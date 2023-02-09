de53417dce3420c8182b4a11decade8431573f78
gustavorteuber <gugareis135@gmail.com>
2023-02-09 00:39:17 -0300

    tailwind-home-finaly

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import heading from "./components/heading.vue";
+import footerinfo from "./components/footer.vue";
+import headerinfo from "./components/header.vue";
+export default {
+  components: { headerinfo, heading, footerinfo },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="bg-neutral-900 justify-center">
+    <headerinfo></headerinfo>
+    <heading></heading>
+    <footerinfo></footerinfo>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/heading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-20"></div>
+  <div class="flex justify-center">
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 md:text-5xl lg:text-6xl"
+    >
+      O Proative chegou!
+    </h1>
+  </div>
+  <p
+    class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+  >
+    Oferecemos soluções logísticas personalizadas para empresas. Com sistemas
+    avançados e equipe capacitada, garantimos eficiência e segurança nas
+    entregas. Entre em contato conosco para otimizar a sua cadeia de
+    suprimentos.
+  </p>
+  <div class="flex justify-center m-10">
+    <a
+      href="#"
+      class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-neutral-700 border border-emerald-600 rounded-lg focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+    >
+      Vamos lá!
+      <svg
+        class="w-5 h-5 ml-2 -mr-1"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </a>
+  </div>
+</template>
